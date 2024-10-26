419adbe6252def878b6817510e206ed67bfe92c4
muhammadwahabarifin <wahabmuhammad691@gmail.com>
2024-10-27 02:09:02 +0700

    loader update

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,23 @@
 import About from "@/components/About";
 import Cta from "@/components/Cta";
 import Hero from "@/components/Hero";
+import Loader from "@/components/Loader";
 import Reviews from "@/components/Reviews";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <About />
-      <Services />
-      <Work />
-      <Reviews />
-      <Cta />
-    </main>
+    <>
+      <main>
+      <Loader />
+        <Hero />
+        <About />
+        <Services />
+        <Work />
+        <Reviews />
+        <Cta />
+      </main>
+    </>
   );
 }
